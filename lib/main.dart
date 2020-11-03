2897ec8c4c3b1f6d82d1813c3f055d949ef77409
rbourque7 <59621985+rbourque7@users.noreply.github.com>
2020-11-03 16:42:11 -0700

    commit1

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,69 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class MyTabbedScopedModelDemo extends StatelessWidget {
+  const MyTabbedScopedModelDemo({this.title, this.color});
 
   final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+    return ScopedModel<MyModel>(
+      model: MyModel(),
+      child: Scaffold(appBar: AppBar( 
+        title: Text(title),
+        ),
+        body: Column(
+          children: <Widgit>[
+          NumbersCarousel(color: color),
+          NumbersLists(),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class Number {
+  Number(this.title, this.image);
+  final String title;
+  final String image;
+}
+
+List<Number> nums = <Number>[
+  //PUT THE TITLE IMAGES IN HERE
+  Number('1', 'assets/img/1-1.jpg'),
+  Number('2', 'assets/img/2-1.jpg'),
+];
+
+List<Number> ones = <Number>[
+  //PUT THE BOTTOM IMAGES IN HERE
+  Number('1-2', 'assets/img/1-1.jpg'),
+  Number('1-3', 'assets/img/2-1.jpg'),
+  Number('1-4', 'assets/img/1-1.jpg'),
+  Number('1-5', 'assets/img/2-1.jpg'),
+  Number('1-6', 'assets/img/2-1.jpg'),
+  Number('1-7', 'assets/img/2-1.jpg'),
+];
+
+List<Number> twos = <Number>[
+  //PUT THE BOTTOM IMAGES IN HERE
+  Number('2-2', 'assets/img/1-1.jpg'),
+  Number('2-3', 'assets/img/2-1.jpg'),
+  Number('2-4', 'assets/img/1-1.jpg'),
+];
+
+class MyModel extends Model {
+  //PRIVATE
+  List<Number> _chosenNumber = ones;
+  //PUBLIC PROP GETTER
+  List<Number> get chosenNumber => _chosenNumber;
+  //PUBLIC PROP SETTER
+  set chosenNumber(List<Number> type) {
+    _chosenNumber = type;
+  //IF NOTHING CHANGED
+  notifyListeners();
+  }
+}
+
+class NumbersCard extends
